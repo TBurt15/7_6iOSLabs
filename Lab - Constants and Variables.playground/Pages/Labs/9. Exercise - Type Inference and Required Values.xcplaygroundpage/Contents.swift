113fc91cc9c4c47f40b934a9478bd10bccbb2800
enrique-lol <enrique.memije.pro@gmail.com>
2023-07-06 15:12:24 -0500

    finished

---
+++ Lab - Constants and Variables.playground/Pages/Labs/9. Exercise - Type Inference and Required Values.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+/*:
+## Exercise - Type Inference and Required Values
+ 
+ Declare a variable called `name` of type `String`, but do not give it a value. Print `name` to the console. Does the code compile? Remove any code that will not compile.
+ */
+print(name)
+//:  Now assign a value to `name`, and print it to the console.
+var name = "Tristen"
+print(name)
+//:  Declare a variable called `distanceTraveled` and set it to 0. Do not give it an explicit type.
+var distanceTraveled = 0
+
+/*:
+[Previous](@previous)  |  page 9 of 10  |  [Next: App Exercise - Percent Completed](@next)
+ */
